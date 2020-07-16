813c47ad64f03152e7ba3488c8812f813e6b0c74
ashwinbittu <ashwin.bittu@gmail.com>
2020-07-16 10:32:08 +1000

    Add files via upload

---
+++ storage/variables.tf
@@ -0,0 +1,30 @@
+
+variable "tf_org" {
+}
+
+variable "tf_host" {
+}
+
+variable "aws_region" {
+}
+
+variable "repave_strategy" {
+}
+
+variable "bucket_name" {
+}
+
+variable "app_env" {
+}
+
+variable "app_name" {
+}
+
+variable "app_csi" {
+}
+
+variable "aws_key" {
+}
+
+variable "aws_sec" {
+}
